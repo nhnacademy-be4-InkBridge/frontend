13064107f193ac885992628a855069bd0d719f9e
minseo <sa4777@naver.com>
2024-03-05 21:02:12 +0900

    feature: SecurityContextHolder 추가완료, 회원 비회원 메인페이지 분리 추가

--- src/main/java/com/nhnacademy/inkbridge/front/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/inkbridge/front/config/SecurityConfig.java
@@ -1,11 +1,12 @@
 package com.nhnacademy.inkbridge.front.config;
 
+import com.nhnacademy.inkbridge.front.adaptor.MemberAdaptor;
 import com.nhnacademy.inkbridge.front.jwt.filter.CustomJwtAuthenticationFilter;
 import com.nhnacademy.inkbridge.front.jwt.filter.CustomLoginAuthenticationFilter;
 import com.nhnacademy.inkbridge.front.jwt.provider.CustomAuthenticationProvider;
 import com.nhnacademy.inkbridge.front.jwt.service.CustomUserDetailService;
-import com.nhnacademy.inkbridge.front.adaptor.MemberAdaptor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,12 +14,12 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.context.SecurityContextHolderFilter;
 
 /**
  * class: SecurityConfig.
@@ -28,7 +29,7 @@ import org.springframework.security.web.context.SecurityContextHolderFilter;
  */
 @Configuration
 @RequiredArgsConstructor
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 public class SecurityConfig {
 
     private final MemberAdaptor memberAdaptor;
@@ -37,31 +38,42 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         AuthenticationManager authenticationManager =
-            authenticationManager(http.getSharedObject(AuthenticationConfiguration.class));
+                authenticationManager(http.getSharedObject(AuthenticationConfiguration.class));
         http
-            .authorizeRequests()
-            .antMatchers("/","/login", "/signup").permitAll()
-            .antMatchers("/mympage/**").hasRole("MEMBER")
-            .antMatchers("/admin/**").hasRole("ADMIN")
-            .anyRequest().permitAll();
+                .authorizeRequests()
+                .antMatchers("/", "/login", "/signup").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/mypage/**").hasRole("MEMBER")
+                .anyRequest().permitAll();
         http
-            .csrf().disable();
+                .csrf().disable();
         http
-            .cors().disable();
+                .cors().disable();
         http
-            .formLogin().disable();
+                .formLogin().disable();
         http
-            .logout().disable();
+                .logout().disable();
         http
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http
-            .addFilterAt(customLoginAuthenticationFilter(authenticationManager),
-                UsernamePasswordAuthenticationFilter.class);
+                .addFilterAt(customLoginAuthenticationFilter(authenticationManager),
+                        UsernamePasswordAuthenticationFilter.class);
         http
-                .addFilterAfter(customJwtAuthenticationFilter(), SecurityContextHolderFilter.class);
+                .addFilterBefore(customJwtAuthenticationFilter(), CustomLoginAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    /**
+     * 사이트 들어갈때마다 리소스 경로때문에 필터가 두번 작동하는것을 무시함.
+     *
+     * @return 리소스 경로는 필터적용 하지 않는 설정
+     */
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
     /**
@@ -77,22 +89,24 @@ public class SecurityConfig {
 
     /**
      * 모든 요청시마다 토큰 체크하는 필터
+     *
      * @return 필터 거친 결과 값
      */
     @Bean
     public CustomJwtAuthenticationFilter customJwtAuthenticationFilter() {
-        return new CustomJwtAuthenticationFilter(memberAdaptor,userDetailService);
+        return new CustomJwtAuthenticationFilter(memberAdaptor, userDetailService);
     }
 
     /**
      * provider 들을 관리하는 인증 매니저 빈 등록
+     *
      * @param authenticationConfiguration 인증 매니저 관리 config
      * @return 인증 매니저
      * @throws Exception 인증 실패 에러
      */
     @Bean
     public AuthenticationManager authenticationManager(
-        AuthenticationConfiguration authenticationConfiguration) throws Exception {
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
@@ -107,12 +121,13 @@ public class SecurityConfig {
 
     /**
      * 로그인 폼으로부터 아이디 비밀번호를 받아 auth 로 보내 jwt 발급받는 필터.
+     *
      * @return 로그인 필터.
      */
     public CustomLoginAuthenticationFilter customLoginAuthenticationFilter(
-        AuthenticationManager authenticationManager) {
+            AuthenticationManager authenticationManager) {
         CustomLoginAuthenticationFilter customLoginAuthenticationFilter = new CustomLoginAuthenticationFilter(
-            memberAdaptor);
+                memberAdaptor);
         customLoginAuthenticationFilter.setFilterProcessesUrl("/auth-login");
         customLoginAuthenticationFilter.setAuthenticationManager(authenticationManager);
         customLoginAuthenticationFilter.setUsernameParameter("email");
