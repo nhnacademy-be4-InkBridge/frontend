737ef875a94aba9d6de37ba8b3794ad92b529941
hun_e <orutaku15@gmail.com>
2024-03-06 12:46:39 +0900

    feat: 포장지 선택 처리

--- src/main/java/com/nhnacademy/inkbridge/front/controller/OrderController.java
+++ src/main/java/com/nhnacademy/inkbridge/front/controller/OrderController.java
@@ -1,11 +1,23 @@
 package com.nhnacademy.inkbridge.front.controller;
 
 import com.nhnacademy.inkbridge.front.dto.order.OrderBookReadResponseDto;
+import com.nhnacademy.inkbridge.front.dto.order.OrderCreateRequestDto;
+import com.nhnacademy.inkbridge.front.dto.wrapping.WrappingReadResponseDto;
+import com.nhnacademy.inkbridge.front.service.AccumulationRatePolicyService;
+import com.nhnacademy.inkbridge.front.service.DeliveryPolicyService;
+import com.nhnacademy.inkbridge.front.service.OrderService;
+import com.nhnacademy.inkbridge.front.service.WrappingService;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * class: OrderController.
@@ -15,24 +27,72 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 @RequestMapping("/orders")
+@RequiredArgsConstructor
+@Slf4j
 public class OrderController {
+
+    private final OrderService orderService;
+    private final AccumulationRatePolicyService accumulationRatePolicyService;
+    private final DeliveryPolicyService deliveryPolicyService;
+    private final WrappingService wrappingService;
 
     /**
      * 주문서 작성 페이지 호출 요청을 처리하는 메소드입니다.
      *
-     * @return 주문서 작성 페이지
+     * @param model Model
+     * @return 주문서 작성 페이지, Cookie에 도서 정보가 존재하지 않는다면 메인페이지로 이동
      */
     @GetMapping
     public String orderView(Model model) {
-        List<OrderBookReadResponseDto> orderBooks = List.of(
-            new OrderBookReadResponseDto(1L, "기호1번 장재훈", 20000L, 17000L, 5, false),
-            new OrderBookReadResponseDto(2L, "기호2번 이민서", 30000L, 28000L, 3, false),
-            new OrderBookReadResponseDto(3L, "기호3번 김준현", 25000L, 24000L, 2, false),
-            new OrderBookReadResponseDto(4L, "기호4번 정승조", 30000L, 27000L, 1, false)
-        );
+        // 도서 정보
+        // 상품 상세페이지, 장바구니에서 쿠키에 담겨서 넘어옴
+        // 값을 모델에 넣은 후 Cookie 삭제
+        model.addAttribute("orderBooks",
+            List.of(
+                new OrderBookReadResponseDto(1L, "블랙 쇼맨과 운명의 바퀴", true, 19800L, 17820L, 1),
+                new OrderBookReadResponseDto(2L, "명탐견 오드리, 예감은 꼬리에서부터", true, 12000L, 10800L, 2),
+                new OrderBookReadResponseDto(3L, "새콤달콤 캐치! 티니핑 홈스쿨 세계 여러 나라: 아시아 편", true, 12000L,
+                    10800L, 3)));
 
-        model.addAttribute("orderBooks", orderBooks);
+        model.addAttribute("deliveryPolicy", deliveryPolicyService.getCurrentPolicy());
+        model.addAttribute("accumulationRatePolicy",
+            accumulationRatePolicyService.getCurrentPolicy());
+        List<WrappingReadResponseDto> wrappingList = wrappingService.getWrappingList();
+        log.info("{}", wrappingList);
+        model.addAttribute("wrappingList", wrappingList);
+
+        // 멤버 포인트 조회 메소드 호출
+
         return "member/orders";
+    }
+
+    /**
+     * 주문 요청을 처리하고 결제 페이지로 이동하는 메소드입니다.
+     *
+     * @param requestDto 주문 정보
+     * @return 결제 페이지
+     */
+    @PostMapping
+    public String createOrder(@ModelAttribute OrderCreateRequestDto requestDto,
+        RedirectAttributes redirectAttributes) {
+        log.info("create order {}", requestDto);
+        // 여기서 주문정보를 저장하고 결제 페이지로 이동 (Return OrderId)
+        // requestDto + principal (memberId or "anonymousUser")
+        // RedirectAttributes paymentInfo 객체 add.
+
+        String principal = (String) SecurityContextHolder.getContext().getAuthentication()
+            .getPrincipal();
+
+        if (!"anonymousUser".equals(principal)) {
+            requestDto.setMemberId(principal);
+        }
+
+        String orderId = orderService.createOrder(requestDto);
+        redirectAttributes.addFlashAttribute("orderId", orderId);
+        // redirectAttributes.addFlashAttribute("paymentInfo", 결제 정보);
+
+        // 결제 페이지로 변경해야함.
+        return "redirect:/orders";
     }
 
 }
---
+++ src/main/resources/static/js/order.js
@@ -0,0 +1,169 @@
+"use strict";
+
+// 페이지 로드 시 결제 정보 초기화 함수
+function paymentInfo() {
+  let sumTotalRegular = 0;
+  let sumTotalPrice = 0;
+
+  orderBooks.forEach(book => {
+    sumTotalRegular += book.regularPrice * book.amount;
+    sumTotalPrice += book.price * book.amount;
+    console.log(book.isPackagable);
+  });
+
+  document.getElementById("total_regular_price").innerText = sumTotalRegular;
+  document.getElementById("product_discount").innerText = sumTotalRegular - sumTotalPrice;
+  document.getElementById("package_price").innerText = '0';
+  document.getElementById("coupon_discount").innerText = '0';
+  document.getElementById("use_point").innerText = '0';
+
+  // 배송비 계산
+  let deliveryPrice = sumTotalPrice < deliveryPolicy.freeDeliveryPrice
+      ? deliveryPolicy.deliveryPrice : 0;
+
+  document.getElementById("delivery_price").innerText = deliveryPrice;
+
+  // 예상 포인트 적립률
+  let accumulatePoint = Math.round(
+      sumTotalPrice * accumulationRatePolicy.accumulationRate / 100);
+
+  document.getElementById("accumulate_point").innerText = accumulatePoint;
+
+  // 결제 금액 = 판매가 + 배송비 + 포장비 - 쿠폰할인 - 사용 포인트
+  document.getElementById("pay_amount").innerText = sumTotalPrice
+      + deliveryPrice;
+}
+
+// 상품 금액 - 상품 할인 금액 계산 함수
+function calcBookPrice() {
+  let sumTotalPrice = 0;
+
+  orderBooks.forEach(book => {
+    sumTotalPrice += book.price * book.amount;
+  });
+
+  return sumTotalPrice;
+}
+
+// 페이지 로드 이벤트 추가
+document.addEventListener("DOMContentLoaded", paymentInfo);
+
+// 포인트 사용 시 event -> onchange
+// 보유 포인트 내에서 사용할 수 있어야 함. (분기문 작성 필요)
+function calcPoint() {
+  let point = document.getElementById("using_point");
+
+  // 아무것도 입력 되지 않은 상태
+  if (point.value === null || point.value === "") {
+    point.value = "0";
+  }
+
+  // 입력값이 허용될 수 없는 문자인지 확인
+  let chkStyle = /^[0-9]+$/;
+  // 상품 금액을 넘어서는지 확인
+  let sumTotalPrice = calcBookPrice();
+  if (sumTotalPrice < parseInt(point.value) || !chkStyle.test(point.value)) {
+    alert("포인트를 다시 입력해 주세요.");
+    point.value = "0";
+  }
+
+  // 보유 포인트 이상 사용 불가 확인
+  document.getElementById("use_point").innerText = point.value;
+  return parseInt(point.value);
+}
+
+// 쿠폰 할인 금액 계산
+function calcCoupon() {
+
+
+  return 0;
+}
+
+// 포장 가격 계산
+function calcWrapping() {
+  let totalWrappingPrice = 0;
+
+  for (let i = 0; i < orderBooks.length; i++) {
+    console.log(i);
+    console.log(orderBooks[i].isPackagable);
+    if (orderBooks[i].isPackagable) {
+      let orderBook = "bookOrderList[" + i + "]";
+      console.log("orderBook" + orderBook);
+      let wrappingId = document.getElementById(orderBook + ".wrappingId");
+      console.log("wrappingId" + wrappingId.value);
+      let bookWrappingPrice = document.getElementById(orderBook + ".wrappingPrice");
+      console.log("bookWrappingPrice" + bookWrappingPrice.value);
+
+      if (wrappingId.value == null || wrappingId.value == "") {
+        continue;
+      }
+
+      let wrappingPrice = getWrappingPrice(wrappingId.value);
+
+      if (wrappingPrice == null) {
+        alert("잘못된 포장지 번호입니다. 다시 선택해주세요.");
+        return null;
+      }
+
+      console.log("wrappingPrice" + wrappingPrice);
+
+      totalWrappingPrice += wrappingPrice;
+
+      bookWrappingPrice.value = wrappingPrice;
+
+      console.log("bookWrappingPrice" + bookWrappingPrice.value);
+    }
+
+  }
+
+  document.getElementById("package_price").innerText = totalWrappingPrice;
+  return totalWrappingPrice;
+}
+
+function getWrappingPrice(id) {
+  for (let i = 0; i < wrappingList.length; i++) {
+    if (wrappingList[i].wrappingId == id && wrappingList[i].isActive) {
+      console.log("일치하는 포장지 찾음")
+      return wrappingList[i].price;
+    }
+  }
+
+  return null;
+}
+
+// 결제 금액 계산 함수
+function calcPayAmount() {
+  let wrappingPrice = calcWrapping();
+  let couponDiscountPrice = calcCoupon();
+  let pointDiscountPrice = calcPoint();
+  let sumBookPrice = calcBookPrice();
+
+  if (wrappingPrice == null || couponDiscountPrice == null || pointDiscountPrice == null) {
+    return;
+  }
+
+  let deliveryPrice  = sumBookPrice < deliveryPolicy.freeDeliveryPrice
+      ? deliveryPolicy.deliveryPrice : 0;
+
+  let totalPrice = sumBookPrice + deliveryPrice + wrappingPrice - couponDiscountPrice - pointDiscountPrice;
+
+  if (totalPrice < 0) {
+    alert("결제 금액은 0원 미만일 수 없습니다.");
+    return;
+  }
+
+  document.getElementById("pay_amount").innerText = totalPrice;
+  return totalPrice;
+}
+
+// 배송 예정일 익일부터 선택 가능
+function setDeliveryDate() {
+  let deliveryDate = document.getElementById("delivery_date");
+
+  let tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+
+  deliveryDate.setAttribute("min", tomorrow.toISOString().split("T")[0]);
+}
+
+document.addEventListener("DOMContentLoaded", setDeliveryDate);
