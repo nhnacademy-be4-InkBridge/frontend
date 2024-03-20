4c67b30daba67ff083df3f3e6131727a21ae36d9
minm063 <jangmin063@gmail.com>
2024-03-20 18:23:05 +0900

    feat: 리뷰 CRU

---
+++ src/main/java/com/nhnacademy/inkbridge/front/dto/book/BookDetailReadResponseDto.java
@@ -0,0 +1,38 @@
+package com.nhnacademy.inkbridge.front.dto.book;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.Set;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * class: BookReadResponseDto.
+ *
+ * @author minm063
+ * @version 2024/02/26
+ */
+@Getter
+@NoArgsConstructor
+public class BookDetailReadResponseDto {
+
+    private String bookTitle;
+    private String bookIndex;
+    private String description;
+    private LocalDate publicatedAt;
+    private String isbn;
+    private Long regularPrice;
+    private Long price;
+    private BigDecimal discountRatio;
+    private Boolean isPackagable;
+    private String thumbnail;
+    private String statusName;
+    private Long publisherId;
+    private String publisherName;
+    private Map<Long, String> authors;
+    private Long wish;
+    private Set<String> fileUrl;
+    private Set<String> tagName;
+    private Set<String> categoryName;
+}
