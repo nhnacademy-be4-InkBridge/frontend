ede7a70b4ed7daf053141deb0c3795ebd24fca38
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2024-03-23 20:50:54 +0900

    refactor: IndexController conflict 해결

--- src/main/java/com/nhnacademy/inkbridge/front/controller/IndexController.java
+++ src/main/java/com/nhnacademy/inkbridge/front/controller/IndexController.java
@@ -19,6 +19,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -34,6 +35,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 @RequestMapping("/")
+@RequiredArgsConstructor
 @Slf4j
 public class IndexController {
 
