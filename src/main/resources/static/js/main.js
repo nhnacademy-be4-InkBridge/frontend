63cbcc9b28bd8e5db64cf4d701026d7e59067a5e
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2024-03-23 20:56:12 +0900

    refactor: index.html conflict 해결

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,21 @@
+const booksContainers = document.querySelectorAll(".books-container");
+
+booksContainers.forEach((booksContainer)=>{
+  const bookInfoContainer=booksContainer.querySelector(".book-info-container");
+
+  let index = 0;
+
+  const prevButton = booksContainer.querySelector(".prev-btn");
+  prevButton.addEventListener("click",()=>{
+      if(index===0) return;
+        index-=1;
+        bookInfoContainer.style.transform = `translate3d(-${270*index}px,0,0)`;
+  })
+
+  const nextButton = booksContainer.querySelector(".next-btn");
+  nextButton.addEventListener("click",()=>{
+    if(index===4) return;
+    index+=1;
+    bookInfoContainer.style.transform = `translate3d(-${270*index}px,0,0)`;
+  })
+})
