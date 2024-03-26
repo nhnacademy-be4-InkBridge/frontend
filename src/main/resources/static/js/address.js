4ec7f42c6793a5fb4b91783683be4fbf67f14b4e
정병훈 <rootofproblem23@gmail.com>
2024-03-26 14:27:33 +0900

    feat: 주문/ 회원 포인트, 주소 기능 추가

--- src/main/resources/static/js/address.js
+++ src/main/resources/static/js/address.js
@@ -28,16 +28,16 @@ function fillAddressFields(selectedDropdown) {
   // '새 주소 입력' 선택시 입력 필드를 비웁니다.
   if(selectedDropdown.value === "") {
     document.getElementById('receiverName').value = "";
-    document.getElementById('receiverPhone').value = "";
+    document.getElementById('receiverNumber').value = "";
     document.getElementById('zipCode').value = "";
     document.getElementById('address').value = "";
     document.getElementById('detailAddress').value = "";
   } else {
     // 선택된 주소의 상세 정보로 입력 필드를 채웁니다.
     document.getElementById('receiverName').value = selectedOption.getAttribute('data-receiver-name');
-    document.getElementById('receiverPhone').value = selectedOption.getAttribute('data-receiver-phone');
     document.getElementById('zipCode').value = selectedOption.getAttribute('data-zip-code');
     document.getElementById('address').value = selectedOption.getAttribute('data-address');
     document.getElementById('detailAddress').value = selectedOption.getAttribute('data-detail-address');
+    document.getElementById('receiverNumber').value = selectedOption.getAttribute('data-receiver-number');
   }
 }
